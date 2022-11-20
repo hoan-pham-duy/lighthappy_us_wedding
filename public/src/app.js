9060b290e963efa7846bb9892ccefd31589dfce8
Pham Duy Hoan <phamduyhoan95@gmail.com>
2022-11-20 10:31:48 +0700

    [#12] First version using firebase

---
+++ public/src/app.js
@@ -0,0 +1,11 @@
+// import { API, graphqlOperation  } from 'aws-amplify';
+// import { listTodos  } from './graphql/queries';
+
+const getWeddingMessages = function(){
+    console.log('Start running getWeddingMessages')
+    //const todos = await API.graphql(graphqlOperation(listTodos));
+}
+
+const clickedButton = function(){
+    console.log('Button is clicked')
+}
